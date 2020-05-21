9633aca05e8381600d99d871892202563dfefa00
rohanC-dev <rohan.chopra241@gmail.com>
2020-05-21 12:56:18 -0400

    added form for searching houses

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ var express = require("express");
 var app = express();
 
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 
-app.get("/", function(req, res){
+app.get("/", function(req, res) {
     res.render("home");
 });
 
-app.listen(process.env.PORT || 3000, process.env.IP, function () {
+app.listen(process.env.PORT || 3000, process.env.IP, function() {
     console.log("server started");
 });
